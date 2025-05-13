da0d78e7be88af4d87986fce1670fc68a18cb53f
Vismen <gopal.vismen@gmail.com>
2025-05-13 10:19:08 +0530

    updated Now

--- app.js
+++ app.js
@@ -197,6 +197,7 @@ app.use(cors({
 
 
 // ✅ Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running at: http://localhost:${PORT}`);
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
 });
