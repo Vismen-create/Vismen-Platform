bd37818724d9a615e3f831653e2e6e1cc037f792
Vismen <gopal.vismen@gmail.com>
2025-05-27 17:14:31 +0530

    Final Fixes

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
